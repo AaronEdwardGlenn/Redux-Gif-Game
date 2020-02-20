fa933f200987be332838c29f460db86383a5a7bb
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-19 20:38:33 -0800

    added gamePage.js

---
+++ src/components/game/gamePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './GamePage.css';
+
+export const GamePage = () => {
+
+  return (
+    <div className={styles.GamePage}>
+      <Link to ='/game'>
+      </Link>
+    </div>
+  );
+
+};
+
